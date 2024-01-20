0bae2da59f421b78678b996c4d344933089169b4
Neeraj <47555491+nksharma063@users.noreply.github.com>
2024-01-20 13:04:26 +0530

    Update variables.tf
    adding syntax

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
+# Variables
+variable "front_end_instance_count" {
+  default = 5
+}
+
+variable "microservices_instance_count" {
+  default = 7
+}
+
+variable "db_instance_count" {
+  default = 2
+}
+
+# Subnet IDs
+variable "subnet_ids" {
+  default = ["subnet-xxxxxxxx", "subnet-yyyyyyyy"]  # Replace with your subnet IDs
+}
 
